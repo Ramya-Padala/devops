b492f563d62ac51a2b8a91ef42b6337213bd3dfc
Ramya-Padala <padalaramya482@gmail.com>
2025-11-04 04:55:18 -0500

    Add package.json, index.js and update workflow

---
+++ index.js
@@ -0,0 +1,12 @@
+{
+  "name": "devops-demo",
+  "version": "1.0.0",
+  "description": "GitHub Actions CI/CD demo project",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Running tests... All tests passed!\"",
+    "build": "echo \"Building project... Done!\" && node index.js"
+  },
+  "author": "Ramya Padala",
+  "license": "MIT"
+}
